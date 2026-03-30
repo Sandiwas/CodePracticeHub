5e3b2a2df4e320f656c1677e0e85cd78e94e5f27
sandiwas <harrycode9@gmail.com>
2026-03-30 12:19:08 +0530

    commited Anagram string Program

---
+++ src/StringCode/P6AnagramString.java
@@ -0,0 +1,21 @@
+package StringCode;
+
+import java.util.Arrays;
+
+public class P6AnagramString {
+	public static void main(String[] args) {
+		String str1 = "army";
+		String str2 = "mary";
+		char[] arr1 = str1.toLowerCase().toCharArray();
+		char[] arr2 = str2.toLowerCase().toCharArray();
+
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+
+		if (Arrays.equals(arr1, arr2)) {
+			System.out.println("Given string is anagarm");
+		} else {
+			System.out.println("given string is not anagram");
+		}
+	}
+}
