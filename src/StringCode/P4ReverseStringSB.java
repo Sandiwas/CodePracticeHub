7339d2d258758fc34295dedf86729710bc61a8ce
sandiwas <harrycode9@gmail.com>
2026-03-30 10:55:06 +0530

    commited reverse string using stringBuffer Program

---
+++ src/StringCode/P4ReverseStringSB.java
@@ -0,0 +1,9 @@
+package StringCode;
+
+public class P4ReverseStringSB {
+	public static void main(String[] args) {
+		String str = "sandip";
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse());
+	}
+}
